55df9c8b2d11cdaf440dbc980b3dd95a261ce181
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-31 23:15:59 +0300

    Create BuildArrayFromPermutation.java

---
+++ BuildArrayFromPermutation/BuildArrayFromPermutation.java
@@ -0,0 +1,19 @@
+package BuildArrayFromPermutation;
+
+/*
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+ */
+
+
+
+public class BuildArrayFromPermutation {
+
+    public int[] buildArray(int[] nums) {
+        int [] newArray = new int[nums.length];
+        for (int i=0;i<nums.length;i++){
+            newArray[i]=nums[nums[i]];
+        }
+        return newArray;
+    }
+}
