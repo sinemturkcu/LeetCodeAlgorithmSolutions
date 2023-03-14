1f1f1f4afbbe2995facea4485223f5a46bd6f7e4
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-03-14 22:47:52 +0300

    Create PlusOne.java

---
+++ PlusOne/PlusOne.java
@@ -0,0 +1,22 @@
+package PlusOne;
+
+
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+
+       for(int i =digits.length-1 ; i >= 0 ; i--){
+
+          if(digits[i]!=9){
+            digits[i]++;
+            return digits;
+          }
+
+          else{
+             digits[i]=0;
+          }
+       }
+       int [] arr=new int[digits.length+1];
+       arr[0]=1;
+       return arr;
+    }
+}
